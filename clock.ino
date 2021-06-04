5d41ce7decc16113a9bf1500849fe2efb41a82aa
KIM-taeseong <dnfrkwhr2424@naver.com>
2021-06-04 10:57:40 +0900

    modify pin number

--- clock.ino
+++ clock.ino
@@ -1,9 +1,11 @@
-void setup(){
+void setup()
+{
     Serial.begin(9600);
-    pinMode(13, OUTPUT);
+    pinMode(12, OUTPUT);
 }
 
-void loop(){
+void loop()
+{
     digitalWrite(13, HIGH);
     delay(500);
     digitalWrite(13, LOW);
