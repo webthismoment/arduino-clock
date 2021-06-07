af6c4d2598fe88c52bfffef34b3b9f4639fd68da
wehthismoment <webthismoment@gmail.com>
2021-06-07 15:01:43 +0900

    핀 수정, sw_state 추가, isSwitchUp/isSwitchDown으로 분기

--- clock.ino
+++ clock.ino
@@ -2,12 +2,19 @@
 #include <TimeLib.h> //https://github.com/PaulStoffregen/Time
 #include "pitches.h"
 
+// A0,A1,A2,A3,A4,A5 == 14,15,16,17,18,19
 #define MODE_CLOCK 0
 #define MODE_TIMER 1
 #define MODE_MODIFY 2
-#define pin_led 17
+#define pin_led 19
 #define pin_buzzer 13
+#define pin_sw_mode 14
+#define pin_sw1 15
+#define pin_sw2 16
+#define pin_sw_cursor 17
 
+int prev_sw_state[] = {0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1, 1,1,1}; //TODO: hashmap으로 변경(14~17만 사용)
+int sw_state[] = {0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,1, 1,1,1};
 int timer= 0;
 int timer_min = 0;
 int timer_sec = 0;
@@ -30,7 +37,22 @@ void displayTimer(int);//타이머 display_타이머모드
 void timer_off();//타이머 리셋
 
 //전역함수//
-int sw11(){ return digitalRead(15); } //스위치1 누르는 순간 LOW, 아니면 HIGH 반환
+
+boolean isSwitchUp(int i){
+  int val = digitalRead(i);
+  if(prev_sw_state[i] == LOW && sw_state[i] == HIGH)
+    return true;
+  else
+    return false;
+}
+
+boolean isSwitchDown(int i){
+  int val = digitalRead(i);
+  if (prev_sw_state[i] == HIGH && sw_state[i] == LOW)
+    return true;
+  else
+    return false;
+}
 
 void timer_off(){
   timer_end_time = 0;
@@ -48,27 +70,23 @@ void setup() {
   pinMode(14, INPUT_PULLUP);  //MODE 변경(시계, 타이머, 시간설정)
   pinMode(15, INPUT_PULLUP);  //타이머 시작, 알람 울리면 종료
   pinMode(16, INPUT_PULLUP);  //타이머 값 증가, 타이머 일시정지/재개
+  pinMode(17, INPUT_PULLUP);  //커서 이동
   pinMode(pin_led, OUTPUT); // LED 
-  pinMode(18, INPUT_PULLUP);  //커서 이동
   
   //Program setup
-  setTime(13,48,20,1,6,2021);  //NOTE: 테스트 전에 현재 시각으로 설정
+  setTime(14,53,20,7,6,2021);  //NOTE: 테스트 전에 현재 시각으로 설정
   //setTime(hr,min,sec,day,month,yr);
-  //setSyncProvider();
 }
 
-
 void loop() {
-  //t = now();
-  int sw_mode = digitalRead(14); //스위치0 누를 동안 LOW, 아니면 HIGH 반환
-  int sw1 = digitalRead(15); //스위치1 누를 동안 LOW, 아니면 HIGH 반환
-  int sw2 = digitalRead(16); //스위치2 누를 동안 LOW, 아니면 HIGH 반환
-  int sw_cursor = digitalRead(18); //스위치3 누를 동안 HIGH, 아니면 LOW 반환
+  for(int i=14;i<=17;i++)
+    sw_state[i] = digitalRead(i);
   //Serial.println(String(sw_mode) + " " + String(sw1) + " " + String(sw2)+ " " + String(sw_cursor));
+  //Serial.println("sw_mode: "+String(prev_sw_state[14]) + "switch_0:" +digitalRead(14));
   
-  if (sw_mode == LOW){  
+  if(isSwitchDown(pin_sw_mode)){
+  //if (sw_mode == LOW){  
     MODE = (MODE + 1) % 3;  
-    delay(300);
   }//스위치0 누르면 모드 변환(시계모드, 타이머모드, 시간설정모드)
   
   switch(MODE){
@@ -82,15 +100,12 @@ void loop() {
       else
         displayClockBlink(timer_min, timer_sec, curs);
 
-      if(sw_cursor == LOW)
+      if(isSwitchDown(pin_sw_cursor))
       {
         curs = (curs + 1) % 4;
-        //Serial.println(curs);
-        //displayClockBlink(timer_min, timer_sec, curs);
-        delay(500);
       }
       
-      if(sw1 == LOW){ 
+      if(isSwitchDown(pin_sw1)){ 
          if(timer_end_time == 0 && timer > 0 ){
           //Serial.println("start:" + String(timer_end_time) + " ,timer:" + String(timer));
           timer_end_time = now() + timer;//timer_end_time에 현재 시간값(now)+타이머 설정한 값 쓰기 // now(): eturns the current time as seconds since Jan 1 1970
@@ -99,40 +114,34 @@ void loop() {
         else{
           timer_off();
         }//타이머 시작 후면 타이머 리셋
-        delay(500);
       }//스위치1 눌렀을 때
       
-      if(sw2 == LOW){
-        //displayTimer(timer);
+      if(isSwitchDown(pin_sw2)){
         if (timer_end_time == 0)
         {
-            if(curs == 0){
-              timer_min += 10;
-              if(timer_min > 99)
-                timer_min = 0;
-            }
-            else if(curs == 1){
-              timer_min += 1;
-              if(timer_min > 99)
-                timer_min = 0;
-            }
-            else if(curs == 2){
-              timer_sec += 10;
-              if(timer_sec >= 60)
-                timer_sec = 0;
-            }
-            else if(curs == 3){
-              timer_sec += 1;
-              if(timer_sec >= 60)
-                timer_sec = 0;
-            }
-            timer = int(timer_min)*60 + int(timer_sec);   
-           // Serial.println(timer);
+          if(curs == 0){
+            timer_min += 10;
+          }
+          else if(curs == 1){
+            timer_min += 1;
+          }
+          else if(curs == 2){
+            timer_sec += 10;
+          }
+          else if(curs == 3){
+            timer_sec += 1;
+          }
+
+          if(timer_min > 99)
+            timer_min = 0;
+          if(timer_sec >= 60)
+            timer_sec = 0;
+          timer = int(timer_min)*60 + int(timer_sec);   
+          // Serial.println(timer);
         }
         else {
           is_paused = !is_paused;
         }//타이머 시작 후면 일시정지/재개 기능
-        delay(500);
       }//스위치2 눌렀을 때
       break;
 
@@ -141,27 +150,21 @@ void loop() {
       int minute_ = minute(); //0~59
       displayClockBlink(hour_, minute_, cur+1); //설정된 커서 위치의 세그먼트 깜빡임
 
-      if (sw_cursor == LOW) {
+      if (isSwitchDown(pin_sw_cursor)) {
         cur = (cur + 1) % 3;
-        delay(500);
       }//스위치3 누르면 커서 위치 바뀜
       
-      if (sw2 == LOW) {
+      if (isSwitchDown(pin_sw2)) {
         if(cur == 0) //커서 위치가 1이면_2번 세그먼트
           hour_ += 1; 
         else if(cur == 1) //커서 위치가 2이면_3번 세그먼트
-          minute_ += 10; 
-          if(minute_ >= 60)
-            hour_ -= 1; 
+          minute_ += 10;  
         else if(cur == 2)//커서 위치가 3이면_4번 세그먼트
-        {
           minute_ += 1;
-          if(minute_ == 60)
-            hour_ -= 1;
-        }
-  
+
+        if(minute_ >= 60)
+          hour_ -= 1;
         setTime(hour_, minute_, second(), day(), month(), year()); //스위치2 누를 때마다 시간 설정 업데이트
-        delay(500);
       }//스위치2 눌렀을 때
       
       break;
@@ -187,7 +190,7 @@ void loop() {
         {
           tone(pin_buzzer, melody[thisNote],noteLength);
           delay(pauseBetweenNotes); //BUZZER on
-          if(sw11() == LOW)
+          if(digitalRead(pin_sw1) == LOW)
           {
             timer_off();
             break;
@@ -201,4 +204,8 @@ void loop() {
     noTone(pin_buzzer);
   }//알람 울리지 않아야 할 경우에는 LED, BUZZER off
 
+  //switch 상태저장
+  for(int i=14;i<=17;i++){
+    prev_sw_state[i] = sw_state[i];
+  }
 }//loop 끝
